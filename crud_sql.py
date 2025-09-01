acc8f85b9f56df78349933b2b5e8931cbe19fd7e
gtapiero <giovanny.tapiero@pi.edu.co>
2025-09-01 11:07:03 -0500

    commit inicial

---
+++ crud_sql.py
@@ -0,0 +1,27 @@
+import pyodbc
+
+def conectar():
+    conexion = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "SERVER=GTAPIERO-POLI;" # Cambia por tu servidor, ej: "localhost\\SQLEXPRESS"
+        "DATABASE=PruebaPython;"
+        "UID=sa;"
+        "PWD=tapiero;"
+    )
+    return conexion
+
+#------------------------
+# CRUD
+#------------------------
+
+# CREATE
+def insertar_usuario(nombre, edad, email):
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("INSERT INTO Usuarios (nombre, edad, email) VALUES (?, ?, ?)",
+                    (nombre, edad, email))
+    conexion.commit()
+    conexion.close()
+    print(f"Usuario {nombre} insertado con exito.")
+
+# READ
